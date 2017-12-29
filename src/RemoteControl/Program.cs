f568ddca4e87e081730de7403440c375abbcd316
desmondnzuza <desmondnzuza@gmail.com>
2017-12-29 09:54:23 +0200

    updates main program to make use of simpleremote control

---
+++ src/RemoteControl/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using RemoteControl.Appliances;
+using RemoteControl.Commands;
+using RemoteControl.Controllers;
+
+namespace RemoteControl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var remote = new SimpleRemoteControl();
+
+            var light = new Light();
+            var lightOn = new LightOnCommand(light);
+
+            var garageDoor = new GarageDoor();
+            var garageOpen = new GarageDoorOpenCommand(garageDoor);
+
+            remote.SetCommand(lightOn);
+            remote.ButtonWasPressed();
+
+            remote.SetCommand(garageOpen);
+            remote.ButtonWasPressed();
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadLine();
+        }
+    }
+}
